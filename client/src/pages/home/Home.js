5d4488fd8e0da66bb8a0902c4b22362680509b6f
AhmadMraish <ahmadmraish59@gmail.com>
2021-12-25 20:38:48 +0200

    lists and fitch movies and data from api

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -2,16 +2,42 @@ import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import "./home.scss";
 import List from "../../components/list/List";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Home = ({type}) => {
+const Home = ({ type }) => {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""} ${
+            genre ? "&genre=" + genre : ""
+          }`,{
+            headers:{
+              token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYzY0MWRiODBmNTM0YmZiYmU1MTk5NSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MDQ1NTE4NiwiZXhwIjoxNjQwODg3MTg2fQ.8If0R-MQUmpY61XrhSLUUrr5b9XhK16u-rMpi2xRqQ8"
+            },
+          }
+        );
+        //  console.log(res.data)
+        setLists(res.data)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists();
+  }, [type, genre]);
   return (
     <div className="home">
       <Navbar />
-      <Featured type={type}/>
-      <List/>
-      <List/>
-      <List/>
-      <List/>
+      <Featured type={type} />
+      {lists.map((list)=>(
+        <List list={list}/>
+
+      ))}
+     
     </div>
   );
 };
