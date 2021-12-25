c7b40ba0d1d4a85c62ebce3fe68a910b80617649
AhmadMraish <ahmadmraish59@gmail.com>
2021-12-25 18:36:38 +0200

    react router dom v6 implemented routing for the website

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,45 @@
-import "./app.scss"
+import "./app.scss";
+import React from "react";
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 const App = () => {
-  return <Home/>;
+  const user = true;
+  // Remember react-router-dom v6  switch replaced with routes   :(
+  // Remember react-router-dom v6 redirect replaced with Navigate
+  return (
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={user ? <Home /> : <Navigate to="/register" />}
+        />
+        <Route
+          exact
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/" />}
+        />
+        <Route
+          exact
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+
+        {user && (
+          <>
+            <Route exact path="/movies" element={<Home type="movies" />} />
+            <Route exact path="/series" element={<Home type="series" />} />
+            <Route exact path="/watch" element={<Watch />} />
+          </>
+        )}
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
