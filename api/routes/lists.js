30455e3ace06b5599206533e81aad311370ef6c2
AhmadMraish <ahmadmraish59@gmail.com>
2021-12-25 07:13:09 +0200

    backedend semi done

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -1,20 +1,62 @@
 const router = require("express").Router();
-const Movie = require("../models/Movie");
+const List = require("../models/List");
 const verify = require("../verifyToken");
 
-//CREATE
+//CREATE LIST
 router.post("/", verify, async (req, res) => {
   if (req.user.isAdmin) {
-    const newMovie = new Movie(req.body);
+    const newList = new List(req.body);
 
     try {
-      const savedMovie = await newMovie.save();
-      res.status(201).json(savedMovie);
+      const savedList = await newList.save();
+      res.status(201).json(savedList);
     } catch (err) {
       res.status(500).json(err);
     }
   } else {
     res.status(403).json("You are not an admin");
+  }
+});
+
+//DELETE LIST
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await List.findByIdAndDelete(req.params.id);
+      res.status(201).json("List has been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not an admin");
+  }
+});
+
+//GET LISTS
+
+router.get("/", verify, async (req, res) => {
+  const typeQuery = req.query.type;
+  const genreQuery = req.query.genre;
+  let list = [];
+  try {
+    if (typeQuery) {
+      if (genreQuery) {
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery, genre: genreQuery } },
+        ]);
+      } else {
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery } },
+        ]);
+      }
+    } else {
+      list = await List.aggregate([{ $sample: { size: 10 } }]);
+    }
+    res.status(200).json(list);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
